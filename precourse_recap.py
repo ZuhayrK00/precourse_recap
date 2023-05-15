61579eb8dc8b6b9e713a09b4a7e59a0f9372f63b
Zuhayr Khan <zuhayrkhan@Zuhayrs-MacBook-Air.local>
2023-05-15 16:15:40 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,16 @@
+num_1 = 3
+num_2 = 4
+num_3 = 1
+num_6 = 2
+
+total = num_2 + num_1 - num_3 + num_6
+print(total)
+
+First_name = "Zuhayr"
+Last_name = "Khan"
+
+Full_name = (First_name + " " + Last_name)
+print(Full_name)
+
+word = "capatilized"
+print(word.capitalize())
